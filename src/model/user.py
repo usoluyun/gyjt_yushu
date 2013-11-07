f9a16f17ec979b637eb3aa3e2dba457fbbd5e4cf
usoluyun <usoluyun@gmail.com>
2013-11-07 17:42:48 +0800

    fix package import issue and pdb debugging problem

---
+++ src/model/user.py
@@ -0,0 +1,13 @@
+""" <docstring here>."""
+#!/usr/bin/python
+#Filename:
+#author: Chester LU <usoluyun@gmail.com>
+
+from db import tdb_sql
+
+def main():
+    """main method."""
+    pass
+
+if __name__ == "__main__":
+    main()
