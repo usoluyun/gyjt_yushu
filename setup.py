d2b8c25c2acf408ff971842889a5e52d28c9f905
usoluyun <usoluyun@gmail.com>
2013-11-11 14:30:40 +0800

    add setup.py file`

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gyjt_yushu',
+    version='0.1',
+    packages=['ysgyjt', 'ysgyjt.db', 'ysgyjt.test', 'ysgyjt.model', 'ysgyjt.network'],
+    url='',
+    license='MIT',
+    author='Chester Lu',
+    author_email='usoluyun@gmail.com',
+    description=''
+)
